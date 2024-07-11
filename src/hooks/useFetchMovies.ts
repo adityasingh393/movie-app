03178bb2fccd304bac9bce5946103e6d3fc3141f
Aditya Singh <aditya.singh@binmile.com>
2024-07-11 13:31:12 +0530

    Fix

--- src/hooks/useFetchMovies.ts
+++ src/hooks/useFetchMovies.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import { setMovies } from '../redux/slices/movieSlice';
+import dummyData from "../data.json"
 
 const useFetchMovies = () => {
   const dispatch = useDispatch();
@@ -9,8 +10,9 @@ const useFetchMovies = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const response = await axios.get('src/data.json');
-        dispatch(setMovies(response.data));
+        // const response = await axios.get('src/data.json');
+        const response = dummyData
+        dispatch(setMovies(response));
       } catch (error) {
         console.error('Error fetching movies:', error);
       }
